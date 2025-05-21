deeee275da0c796a892c58aff49c93b671ca3405
namrata <namrataap725@gmail.com>
2025-05-21 16:03:33 +0545

    docs: add README and example code for React component import/export

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import Header from "./Header.JSX"
+import Login from "./UserComponent" 
+import {Profile} from "./UserComponent"
+import {Settings} from "./UserComponent"
+// import { Header } from "./Header.jsx" // named import
+// import { Login } from "./UserComponent" // named import
 
 function App() {
   return (
     
     <div>
       <Header />
-      <h1>hello react</h1>
-      <h1>My name is namrata</h1>
+      <Login />
+      <hr />
+      <Profile />
+      <hr />
+      <Settings />
+      <hr />
+      <h4>hello react</h4>
+      <h4>My name is namrata</h4>
 
       <FruitList />
       <hr />
---
+++ src/UserComponent.jsx
@@ -0,0 +1,26 @@
+function Login(){
+    return (
+        <div>
+            <h4>Login</h4>
+        </div>
+    );
+}
+
+function Profile(){
+    return (
+        <div>
+            <h4>Profile</h4>
+        </div>
+    );
+}
+
+function Settings(){
+    return (
+        <div>
+            <h4>Settingsssss</h4>
+        </div>
+    );
+}   
+
+export default Login; //we can only export one default component
+export {Profile, Settings};
